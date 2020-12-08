836ce422fdfdf5c0e1938a0c5615dee37b740af3
abugi <abugiboi@gmail.com>
2020-12-08 12:05:33 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <h1>Users</h1>
+    <FormComponent @userInfo="sendUserInformation" :createdUser="newUser" />
+    <ul v-for="person in persons" :key="person.id">
+      <li>
+        <p>Name: {{person.name}}</p>
+        <p>Email: {{person.email}}</p>
+        <p>Phone: {{person.phone}}</p>
+        <div>Company: 
+          <p>Name: {{person.company.name}}</p>
+          <p>Catch Phrase: {{person.company.catchPhrase}}</p>
+          <p>Business Structure: {{person.company.bs}}</p>
+
+        </div>
+      </li>
+    </ul>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormComponent from '@/components/Form'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FormComponent
+  },
+  data() {
+    return {
+      persons: null,
+      newUser: null
+    }
+  },
+  methods: {
+    async sendUserInformation(data) {
+      const url = 'https://jsonplaceholder.typicode.com/users'
+
+      const response = await axios.post(url, data)
+
+      console.log(response)
+      if(response.status === 201) {
+        this.newUser = response.data
+      }
+    }
+  },
+  async created() {
+    try {
+      const url = 'https://jsonplaceholder.typicode.com/users'
+
+      const result = await axios.get(url)
+
+      if(result.status === 200) {
+        this.persons = result.data
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 </script>
@@ -25,4 +70,14 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+ul {
+  list-style-type: none;
+}
+
+li {
+  margin: 3rem;
+  background: #eee;
+  padding: 1rem;
+};
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,47 @@
+<template>
+    <main>
+        <form>
+            <div>
+                <input type="text" placeholder="Name" v-model="name">
+            </div>
+            <div>
+                <input type="email" placeholder="email" v-model="email">
+            </div>
+            <div>
+                <input type="text" placeholder="phone number" v-model="phone">
+            </div>
+        </form>
+        <button @click="submitForm">Submit</button>
+
+        <div>{{createdUser}}</div>
+    </main>
+</template>
+
+<script>
+export default {
+    props: ['createdUser'],
+    data() {
+        return {
+            name: null,
+            email: null,
+            phone: null
+        }
+    },
+    methods: {
+        submitForm() {
+            const data = {
+                name: this.name,
+                email: this.email,
+                phone: this.phone
+            }
+
+            this.$emit('userInfo', data)
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
